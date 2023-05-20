320e991006e70fd159a6325ef8da3c8f7fbd4d70
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-21 01:47:37 +0300

    feat: create Models for - Bus, Passener, Reservation, Route, Seat, and Schedule - Defiened the required relation for each model

---
+++ src/app/Models/Reservation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+    protected $table = 'reservation';
+
+    const STATUS_PENDING = 'pending';
+    const STATUS_APPROVED = 'approved';
+    const STATUS_REJECTED = 'rejected';
+    const STATUS_CANCELED = 'canceled';
+
+
+    public function passenger()
+    {
+        return $this->belongsTo(Passenger::class);
+    }
+
+    public function schedule()
+    {
+        return $this->belongsTo(Schedule::class);
+    }
+
+    public function seat()
+    {
+        return $this->belongsTo(Seat::class);
+    }
+}
