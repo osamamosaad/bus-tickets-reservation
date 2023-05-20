44001e4bc9ebfbb99d4b7cfa473cef3f1559f2bc
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-21 01:45:17 +0300

    feat: Add columns to Passengar & route & reservations - set email as unique in passengar - add status column as enum in reservation - add name in route table

---
+++ src/database/migrations/2023_05_20_191428_add_column_status_for_reservation_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Reservation;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Add column status to reservation table after seat_id column
+        Schema::table('reservation', function (Blueprint $table) {
+            $table->enum('status', [
+                Reservation::STATUS_PENDING,
+                Reservation::STATUS_APPROVED,
+                Reservation::STATUS_REJECTED,
+                Reservation::STATUS_CANCELED,
+            ])->default(Reservation::STATUS_PENDING)
+            ->after('seat_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reservation', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
