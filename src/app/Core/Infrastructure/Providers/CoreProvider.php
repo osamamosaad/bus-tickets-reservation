732f5b46a468da25d1e1c77ae51b9b5d3089a7f7
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-25 02:45:34 +0300

    feat: Supported tools for the core - Register Log queries in laravel log - Create Request Adapter for the core domain

---
+++ src/app/Core/Infrastructure/Providers/CoreProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Core\Infrastructure\Providers;
+
+use App\Core\Infrastructure\Adapters\Request;
+use App\Core\Libraries\Common\RequestInterface;
+use Illuminate\Support\ServiceProvider;
+
+class CoreProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(RequestInterface::class, Request::class);
+    }
+}
