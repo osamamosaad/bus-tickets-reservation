1d8786e3f775d4ea8b76300748ee2ea86f28a305
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-24 08:29:27 +0300

    feat: Add log queries in the DB config and register the service provider

--- src/config/database.php
+++ src/config/database.php
@@ -61,6 +61,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            "log_queries" => env('MYSQL_LOG_QUERIES', false),
         ],
 
         'pgsql' => [
