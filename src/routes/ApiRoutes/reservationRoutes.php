cf14769f69a3cc5dec614d130518d9be55c0a2a5
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-24 18:26:44 +0300

    feat: Add the main routes for reservations, and registered ApiRoutes sub directory

---
+++ src/routes/ApiRoutes/reservationRoutes.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\ReservationController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/reservations', [ReservationController::class, 'list']);
+
+Route::get('/reservations/{id}', [ReservationController::class, 'get']);
+
+Route::post('/reservations', [ReservationController::class, 'create']);
+
+Route::put('/reservations/{id}', [ReservationController::class, 'update']);
+
+Route::delete('/reservations/{id}', [ReservationController::class, 'delete']);
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// Load all routes from the ApiRoutes folder
+$files = File::allFiles(__DIR__ . '/ApiRoutes');
+foreach ($files as $file) {
+    require_once $file->getPathname();
+}
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
