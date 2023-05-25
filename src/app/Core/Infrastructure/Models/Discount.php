042d441332a8792c6f83f79f45e01ed0cea73412
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-25 18:00:13 +0300

    feat: create DB migration and model

---
+++ src/app/Core/Infrastructure/Models/Discount.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Core\Infrastructure\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Discount extends Model
+{
+    use HasFactory;
+    protected $table = 'discount';
+}
