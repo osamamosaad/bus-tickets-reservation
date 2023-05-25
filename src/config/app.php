3b403e1e4df3d982e7391d90173a3e6ecf5b2ed3
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-25 04:05:25 +0300

    feat: registered Core & Repositories Providers

--- src/config/app.php
+++ src/config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Core\Infrastructure\Providers\CoreProvider::class,
+        App\Core\Infrastructure\Providers\RepositoriesProvider::class,
     ])->toArray(),
 
     /*
