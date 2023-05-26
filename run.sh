67a839b0ceff9dc4f4bca3cca53d2d920b2e94e6
Osama Mosaad <osama.mosaad@basharsoft.com>
2023-05-26 05:07:14 +0300

    fix: fix permission laravel issue, and fix command run unittest

--- run.sh
+++ run.sh
@@ -33,8 +33,18 @@ docker exec -it carefer-php-dev php artisan db:seed
 echo "DB Seed Done.\n"
 
 echo "\nRun Unit tests."
-docker exec -it carefer-php-dev php artisan db:seed
+docker exec -it carefer-php-dev php artisan test
 echo "Unit tests Done.\n"
+
+echo "\nClear Laravel cache."
+docker exec -it carefer-php-dev php artisan config:cache
+docker exec -it carefer-php-dev php artisan cache:clear
+echo "Clear Laravel cacheDone.\n"
+
+echo "\nChange storage permission."
+docker exec -it carefer-php-dev chgrp -R www-data storage bootstrap/cache
+docker exec -it carefer-php-dev chmod -R ug+rwx storage bootstrap/cache
+echo "Change storage permission Done.\n"
 
 echo "\n\n"
 echo "\t\t\t\033[43m\033[1;30mApp Domain: \033[0m \033[1;33m http://localhost:$CAREFER_HOST_PORT \033[0m"
